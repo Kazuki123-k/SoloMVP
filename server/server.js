1a6615446d3d9fb7ec6eff410abd6c699400101e
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2022-11-12 16:45:14 +0900

    feat: connect local repo with remote repo

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const db = require("../db/knex");
+const path = require("path");
 
 function setupServer() {
   const app = express();
 
   //middleware
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
   app.use(express.json());
 
   app.get("/hello", (req, res) => {
