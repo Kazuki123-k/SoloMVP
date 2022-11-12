9b1fbcbd2322b45a6f44c4dfa1039d0c4bb1eced
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2022-11-12 17:34:41 +0900

    chore: debug migration problem

--- db/seeds/food_table.js
+++ db/seeds/food_table.js
@@ -6,8 +6,8 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("food").del();
   await knex("food").insert([
-    { id: 1, colName: "rowValue1" },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" },
+    { id: 1, first_name: "test1", last_name: "test1" },
+    { id: 2, first_name: "test2", last_name: "test2" },
+    { id: 3, first_name: "test3", last_name: "test3" },
   ]);
 };
