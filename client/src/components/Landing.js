1dd46a7534f197980ecf23d92a713dd9441a1ff3
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2022-11-16 17:07:56 +0900

    feat: add styling on landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,13 +1,25 @@
-import { useState, useRef, useEffect } from "react";
-import image1 from "../assets/junk_food_cravings.webp";
+import "./Landing.css";
 import { useNavigate, Navigate } from "react-router-dom";
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <img src={image1} />
-      <button onClick={() => navigate("/home")}>Let's Start!</button>
+    <div className="Landing">
+      <div className="content">
+        <h1 className>Alternatives</h1>
+        <h2>
+          The average daily calory intake is about 2,500 for male, and 2,000 for
+          female
+        </h2>
+        <h3>You can easily overconsume calories from eating fast food</h3>
+        <h4>
+          Before you grab that unhealthy meal, see what Alternatives are
+          availabe for you :)
+        </h4>
+        <button className="button" onClick={() => navigate("/home")}>
+          Let's Start!
+        </button>
+      </div>
     </div>
   );
 };
