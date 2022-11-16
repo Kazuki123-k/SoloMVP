f92c59b71775088c1e4978eed7160f7254eacebc
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2022-11-16 13:39:10 +0900

    feat: create List component to show chosen fruit after post request

--- client/src/components/Healthy.js
+++ client/src/components/Healthy.js
@@ -1,16 +1,21 @@
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "./Healthy.css";
+import List from "./List";
 
 const Healthy = () => {
   const [healthyOption, setHealthyOption] = useState([]);
   const [totalCalories, setTotalCalories] = useState(0);
-  const [postObject, setPostObject] = useState([]);
+  const [clickedFruit, setClickedFruit] = useState(null);
+  const [clicked, setClicked] = useState(false);
   const postObj = useRef([]);
 
   const postRequest = async () => {
-    await axios.post(`/chosenFruit`, () => {});
+    await axios
+      .post(`/submitFruit`, postObj.current)
+      .then((response) => console.log(response.data));
   };
+
   const fruitInfo = healthyOption.map((fruit, index) => {
     return (
       <div
@@ -24,6 +29,7 @@ const Healthy = () => {
             calories: fruit.nutritions.calories,
           });
           console.log(postObj.current);
+          setClickedFruit(`${fruit.name} :  ${fruit.nutritions.calories}`);
         }}
         key={index}
       >
@@ -41,12 +47,36 @@ const Healthy = () => {
 
   return (
     <div>
-      <h2>Healthy Options</h2>
-      <button className="health-button" onClick={getFruit}>
-        Get Some Healthy Food!
-      </button>
-      <h3>Current Calories: {totalCalories}</h3>
-      <div className="healthy-container">{fruitInfo}</div>
+      <h2 className="health">Healthy Options</h2>
+      {!clicked && (
+        <button
+          className="health-button"
+          onClick={(e) => {
+            getFruit();
+            setClicked(true);
+          }}
+        >
+          Get Some Healthy Food!
+        </button>
+      )}
+      {clicked && (
+        <div>
+          <h3>Current Calories: {totalCalories}</h3>
+          <button
+            onClick={(e) => {
+              postRequest();
+            }}
+          >
+            Submit
+          </button>
+          <div className="outer-grid">
+            <div className="healthy-container">{fruitInfo}</div>
+            <div className="chosen-fruit">
+              <List chosenFruit={postObj} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
